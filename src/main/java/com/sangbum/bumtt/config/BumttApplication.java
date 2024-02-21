72256a40d8e3082a5b2a771c53d21840015509a7
박상범 <sangbum0919@gmail.com>
2024-02-22 02:58:09 +0900

    [init]깃 연동 테스트 및 초기 설정

---
+++ src/main/java/com/sangbum/bumtt/config/BumttApplication.java
@@ -0,0 +1,13 @@
+package com.sangbum.bumtt.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BumttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sangbum/bumtt/config/BumttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangbum.bumtt.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
